4164e3b6250464b32907d7403fb8b98a7570cef7
victoriawiley1992@gmail.com <victoriawiley1992@gmail.com>
2025-09-22 07:08:38 -0500

    Added readme.md to song exercise for java review

---
+++ Song Exercise Java Review/READMEforSONGEXER.md
@@ -0,0 +1,7 @@
+<h1>Victoria's Code Explination</h1>
+Key Glocks' Chromosones Song 
+Covered by CONSOLE LOG! 
+<br>
+I started by doing the structure of the song, created the file, and wrote out the lines, Then I Simplified the hook with a refrain. Then used a string to make the refractured song. 
+<br>
+Finished up by running log test, and made nessecary visual tweaks for seamless code reading.
---
+++ Song Exercise Java Review/Song exercise.js
@@ -0,0 +1,40 @@
+function printedSong() {
+  console.log("One, two, three, four,");
+  console.log("O's inside a Stryofoam, (Wock', Wock') ");
+  console.log("One, two, three, four,");
+  console.log("hoes who tryna take me home (yeah), ");
+  console.log("Five, six bracelets on my wrist, ");
+  console.log("lookin like a pot of gold (Bitch), ");
+  console.log("I got money, money, money, money runnin' through my chromosomes (yeah),");
+}
+
+function refrain() {
+  console.log("One, two, three, four, ");
+}
+
+function song() {
+  refrain();
+  console.log("O's inside a Stryofoam, (Wock', Wock') ");
+  refrain();
+  console.log("hoes who tryna take me home (yeah), ");
+  console.log("Five, six bracelets on my wrist, ");
+  console.log("lookin like a pot of gold (Bitch), ");
+  console.log("I got money, money, money, money runnin' through my chromosomes (yeah),");
+}
+
+song();
+
+function refactoredSong() {
+  return "One, two, three, four, ";
+}
+
+function refactoredSong() {
+    console.log(refrain());
+    console.log("O's inside a Stryofoam, (Wock', Wock') ");
+    console.log(refrain());
+    console.log("hoes who tryna take me home (yeah), ");
+    console.log("Five, six bracelets on my wrist, ");
+    console.log("lookin like a pot of gold (Bitch), ");
+    console.log("I got money, money, money, money runnin' through my chromosomes (yeah), ");
+}
+refactoredSong();
