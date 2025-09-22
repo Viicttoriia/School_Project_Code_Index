a7b4f3b528ccdd86702613ab25fb824c765e1d85
victoriawiley1992@gmail.com <victoriawiley1992@gmail.com>
2025-09-22 06:31:30 -0500

    Song Exercise Update and Fix

--- Song eXERCISE.js
+++ Song eXERCISE.js
@@ -1,28 +1,29 @@
-title "Song Exercise"
-description "An exercise to practice using classes and objects in JavaScript."
-
 function printedSong() {
-    console.log ("One, two, three, four, /n");
-    console.log ("O's inside a Stryofoam, (Wock', Wock') /n");
-    console.log ("One, two, three, four, /n");
-    console.log ("hoes who tryna take me home (yeah), /n");
-    console.log ("Five, six bracelets on my wrist, /n");
-    console.log ("lookin like a pot of gold (Bitch), /n");
-    console.log ("I got money, money, money, money runnin' through my chromosomes (yeah), /n");
+  console.log("One, two, three, four, /n");
+  console.log("O's inside a Stryofoam, (Wock', Wock') /n");
+  console.log("One, two, three, four, /n");
+  console.log("hoes who tryna take me home (yeah), /n");
+  console.log("Five, six bracelets on my wrist, /n");
+  console.log("lookin like a pot of gold (Bitch), /n");
+  console.log(
+    "I got money, money, money, money runnin' through my chromosomes (yeah), /n"
+  );
 }
 
 function refrain() {
-    console.log ("One, two, three, four, /n");
+  console.log("One, two, three, four, /n");
 }
 
 function song() {
-    refrain();
-    console.log ("O's inside a Stryofoam, (Wock', Wock') /n");
-    refrain();
-    console.log ("hoes who tryna take me home (yeah), /n");
-    console.log ("Five, six bracelets on my wrist, /n");
-    console.log ("lookin like a pot of gold (Bitch), /n");
-    console.log ("I got money, money, money, money runnin' through my chromosomes (yeah), /n");
+  refrain();
+  console.log("O's inside a Stryofoam, (Wock', Wock') /n");
+  refrain();
+  console.log("hoes who tryna take me home (yeah), /n");
+  console.log("Five, six bracelets on my wrist, /n");
+  console.log("lookin like a pot of gold (Bitch), /n");
+  console.log(
+    "I got money, money, money, money runnin' through my chromosomes (yeah), /n"
+  );
 }
 
 song();
